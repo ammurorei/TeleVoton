b713c916d000d973385c6205bb6fd506d2377745
Isac <isac.petruzzi@zemanta.com>
2012-09-12 16:11:51 +0200

    templates to webkitview

--- webkitview/main.cpp
+++ webkitview/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     Html5ApplicationViewer viewer;
     viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
     viewer.showExpanded();
-    viewer.loadFile(QLatin1String("html/index.html"));
+    viewer.loadFile(QLatin1String("html/welcome_video.html"));
 
     return app.exec();
 }
